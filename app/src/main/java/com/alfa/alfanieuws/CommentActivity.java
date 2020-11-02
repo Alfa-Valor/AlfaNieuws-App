f1d325a5529e8f39572396357b7442aa5c924357
christianletteboer <christianletteboer@christian-ict.nl>
2020-11-02 23:48:11 +0100

    added code comments and made it that the comments could be max 250 chars.

--- app/src/main/java/com/alfa/alfanieuws/CommentActivity.java
+++ app/src/main/java/com/alfa/alfanieuws/CommentActivity.java
@@ -5,6 +5,7 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.database.Cursor;
 import android.os.Bundle;
+import android.text.InputFilter;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -24,8 +25,7 @@ import java.util.HashMap;
 
 public class CommentActivity extends AppCompatActivity {
 
-//TODO CODE COMMENTS
-
+    // We initialize the elements used in this activity here
     String postId = "";
     SqlLiteHelper db;
     Context context = this;
@@ -33,12 +33,13 @@ public class CommentActivity extends AppCompatActivity {
     ArrayList<HashMap<String, String>> dataList = new ArrayList<HashMap<String, String>>();
 
 
+    // we set all the elements to use later here. And check for an intent called post_id.
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_comment);
-        listView = (ListView) findViewById(R.id.listView);
 
+        listView = (ListView) findViewById(R.id.listView);
         db = new SqlLiteHelper(context);
 
         Intent intent = getIntent();
@@ -53,6 +54,8 @@ public class CommentActivity extends AppCompatActivity {
     }
     //TODO fetch comments from the API? To fill the comments page with
 
+    // in fetchData() we will get all the comments and put them in the dataList
+
     public void fetchData() {
         Cursor res = db.getAllComments(postId);
         if (res.getCount() == 0) {
@@ -65,17 +68,19 @@ public class CommentActivity extends AppCompatActivity {
                 map.put("name", res.getString(res.getColumnIndex("name")));
                 map.put("text", res.getString(res.getColumnIndex("text")));
                 dataList.add(map);
-            } while(res.moveToNext());
+            } while (res.moveToNext());
         }
-        System.out.println(dataList);
         onPostProcess();
     }
 
+    // iconClicked()
+    // holds the dialog, layout comment_form
     public void iconClicked(View view) {
 
         View dialogView = View.inflate(context, R.layout.comment_form, null);
         final EditText comment_name_box = dialogView.findViewById(R.id.comment_name_box);
         final EditText comment_text_box = dialogView.findViewById(R.id.comment_text_box);
+        comment_text_box.setFilters(new InputFilter[]{new InputFilter.LengthFilter(250)});
 
         final AlertDialog dialog = new AlertDialog.Builder(this)
                 .setView(dialogView)
@@ -93,29 +98,32 @@ public class CommentActivity extends AppCompatActivity {
                     @Override
                     public void onClick(View view) {
 
+                        // when we push the positive button we will check the data filled by the user with the InputValidatorHelper();
                         InputValidatorHelper inputValidatorHelper = new InputValidatorHelper();
                         boolean allowSave = true;
                         String user_name = comment_name_box.getText().toString();
                         String comment = comment_text_box.getText().toString();
 
+                        // A name can't contain any numbers and may not be empty.
                         if (!inputValidatorHelper.isValidName(user_name) || inputValidatorHelper.isNullOrEmpty(user_name)) {
-                            comment_name_box.setError("Een naam mag geen nummers bevatten, tenzij je Willem de 3e bent");
+                            comment_name_box.setError("Uw naam is leeg of onjuist..");
                             allowSave = false;
                         }
 
+                        // A comment without text isn't a comment right ...
                         if (inputValidatorHelper.isNullOrEmpty(comment)) {
-                            comment_text_box.setError("Een reactie zonder tekst.. Dat gaat zomaar niet.");
+                            comment_text_box.setError("U bent uw reactie vergeten..");
                             allowSave = false;
                         }
 
+                        // if we allow to save the comment will be added to the database. We will hide the dialog and "refresh" the activity to add the comment directly cool ha?
                         if (allowSave) {
                             //TODO API or JSON. For now SQLITE is used to add the comment.
                             //TODO ADD toast to let the user know the comment is placed..
-
                             db.addComment(user_name, postId, comment);
                             dialog.dismiss();
-                            onPostProcess();
-
+                            finish();
+                            startActivity(getIntent());
                         }
                     }
                 });
@@ -124,9 +132,9 @@ public class CommentActivity extends AppCompatActivity {
         dialog.show();
     }
 
+    // Here is where the magic happens, we push the data to the adapter in order to get it on the screen.
     public void onPostProcess() {
-        CommentAdapter adapter = new CommentAdapter(context, listView, dataList);
-        System.out.println("DATALIST ONPOST" + dataList);
+        CommentAdapter adapter = new CommentAdapter(context, dataList);
         listView.setAdapter(adapter);
     }
 }
